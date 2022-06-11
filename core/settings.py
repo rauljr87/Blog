f3555a0043b97bd4340161c7f35c5bc4ec1bc852
r4u1 <raul.iturraldec@gmail.com>
2022-06-11 17:42:32 -0500

    core/urls
    - Import settings y static
    - settings debug
    - se realiza migración (python manage.py migrate)

--- core/settings.py
+++ core/settings.py
@@ -119,8 +119,8 @@ STATIC_URL = '/static/'
 # Agregando STATICS URLS
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = [os.path.join(BASE_DIR, 'static_root')]
-MEDIA_ROOT = [os.path.join(BASE_DIR, 'media_root')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
