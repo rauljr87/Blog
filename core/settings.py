658fa87b967f0f69b99f76907b323581ae462a57
r4u1 <raul.iturraldec@gmail.com>
2022-06-11 17:31:34 -0500

    Se agregan statics urls
    - settings.py

--- core/settings.py
+++ core/settings.py
@@ -115,7 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# Agregando STATICS URLS
+MEDIA_URL = '/media/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = [os.path.join(BASE_DIR, 'static_root')]
+MEDIA_ROOT = [os.path.join(BASE_DIR, 'media_root')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
