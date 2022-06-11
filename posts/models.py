effeb9d2a234c600c137b566a1d53045598e3a5b
r4u1 <raul.iturraldec@gmail.com>
2022-06-11 18:16:29 -0500

    se crea modelo PostView
    - Indica cuantas vistas ha tenido el post

--- posts/models.py
+++ posts/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 
 class Post(models.Model):
+    """ Campos para los Posts """
+
     title = models.CharField(max_length=100)
     content = models.TextField()
     thumbnail = models.ImageField()
     publish_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
+
     # author = models.ForeignKey()
 
     def __str__(self):
@@ -14,10 +17,24 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
+    """ Campos para los comentarios """
+
     # user = models.ForeignKey()
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+
+
+class PostView(models.Model):
+    """ Indica cuantas vistas ha tenido el post """
+
+    # user = models.ForeignKey()
+    # hacemos referencia al post con el que estamos haciendo los views
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.user.username
