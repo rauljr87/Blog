ddbcbce004265ddf5673277c54852b42aefdda5a
r4u1 <raul.iturraldec@gmail.com>
2022-06-11 18:07:36 -0500

    se crea modelo Post

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    thumbnail = models.ImageField()
+    publish_date = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    # author = models.ForeignKey()
+
+    def __str__(self):
+        return self.title
