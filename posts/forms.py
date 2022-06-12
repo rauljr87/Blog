867a92e01e655afa40e8a876f0e7b467857c744f
r4u1 <raul.iturraldec@gmail.com>
2022-06-12 18:16:49 -0500

    se configura content en formulario CommentForm

--- posts/forms.py
+++ posts/forms.py
@@ -10,6 +10,9 @@ class PostForm(forms.ModelForm):
 
 # Agregar comentarios
 class CommentForm(forms.ModelForm):
+    content = forms.CharField(required=True, widget=forms.Textarea(attrs={
+        'rows': 4
+    }))
     class Meta:
         model = Comment
         fields = ('content',)
