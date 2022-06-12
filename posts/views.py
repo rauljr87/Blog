115299f9d6dd0887c3a04f63a7b9705cf65e6b46
r4u1 <raul.iturraldec@gmail.com>
2022-06-11 23:30:44 -0500

    Definiendo vistas basadas en clases
    - PostListView
    - PostDetailView
    -  PostCreateView
    - PostUpdateView
    - PostDeleteView

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,23 @@
+# Vistas basadas en clases
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+
+
+class PostDeleteView(DeleteView):
+    model = Post
