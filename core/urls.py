a0bd1720f31bb8e39087429d860bfc30e8ab7f98
r4u1 <raul.iturraldec@gmail.com>
2022-06-11 23:39:22 -0500

    Mapeo de views en core/urls
    - PostListView
    - PostDetailView
    - PostUpdateView
    - PostDeleteView
    - PostCreateView

--- core/urls.py
+++ core/urls.py
@@ -5,10 +5,25 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from posts.views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', PostListView.as_view(), name='list'),
+    path('<slug>/', PostDetailView.as_view(), name='detail'),
+    path('<slug>/update/', PostUpdateView.as_view(), name='update'),
+    path('<slug>/delete/', PostDeleteView.as_view(), name='delete'),
+    path('create/', PostCreateView.as_view(), name='create'),
 ]
+
 
 # settings Debug
 if settings.DEBUG:
