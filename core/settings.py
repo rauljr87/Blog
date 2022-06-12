74fdc9ffca03eb813ee7f30383fd31e11b0ec685
r4u1 <raul.iturraldec@gmail.com>
2022-06-11 19:06:15 -0500

    Installed_apps - allauth

--- core/settings.py
+++ core/settings.py
@@ -36,6 +36,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
     'core',
     'posts'
 ]
@@ -133,7 +138,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
-
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
