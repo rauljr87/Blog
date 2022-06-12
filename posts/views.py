2edc842f4aa4e14aa621db675c27101b94c133d0
r4u1 <raul.iturraldec@gmail.com>
2022-06-11 23:43:10 -0500

    Importando modelos en post/views
    - Post, Comment, PostView, Like

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 # Vistas basadas en clases
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post, Comment, PostView, Like
 
 
 class PostListView(ListView):
