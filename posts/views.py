40962aa97865ff5be84af492a32e411dbe6da077
r4u1 <raul.iturraldec@gmail.com>
2022-06-12 18:21:42 -0500

    funcionalidad para cuando formulario está vacío
    - retorna a la página de detail
    - slug = self.get_object().slug

--- posts/views.py
+++ posts/views.py
@@ -23,6 +23,7 @@ class PostDetailView(DetailView):
             comment.post = post
             comment.save()
             return redirect("detail", slug=post.slug)
+        return redirect("detail", slug=self.get_object().slug)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
